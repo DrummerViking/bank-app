cce43f6d1f5eb718feff9da12f1f54b2b78b4bd2
Your Name <you@example.com>
2022-11-09 02:58:07 -0300

    update app

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 ﻿const routes = {
+    '/': { templateId: 'login' },
     '/login': { templateId: 'login' },
     '/dashboard': { templateId: 'dashboard' },
-    '/404': { templateId: '404' }
+    '/notfound': { templateId: 'notFound' }
 };
-
-function navigate(path) {
-    window.history.pushState({}, path, path);
-    updateRoute();
-}
 
 function updateRoute() {
     const path = window.location.pathname;
     const route = routes[path];
 
     if (!route) {
-        return navigate('/login');
+        return navigate('/notfound');
     }
-  
+
     const template = document.getElementById(route.templateId);
     const view = template.content.cloneNode(true);
     const app = document.getElementById('app');
@@ -24,6 +20,42 @@ function updateRoute() {
     app.appendChild(view);
 }
 
+function navigate(path) {
+    window.history.pushState({}, path, path);
+    updateRoute();
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+}
 
 window.onpopstate = () => updateRoute();
 updateRoute();
+
+async function register() {
+    const registerForm = document.getElementById('registerForm');
+    const formData = new FormData(registerForm);
+    const data = Object.fromEntries(formData);
+    const jsonData = JSON.stringify(data);
+    const result = await createAccount(jsonData);
+
+    if (result.error) {
+        return console.log('An error occurred:', result.error);
+    }
+
+    console.log('Account created!', result);
+}
+
+async function createAccount(account) {
+    try {
+        const response = await fetch('//localhost:5000/api/accounts', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: account
+        });
+        return await response.json();
+    } catch (error) {
+        return { error: error.message || 'Unknown error' };
+    }
+}
